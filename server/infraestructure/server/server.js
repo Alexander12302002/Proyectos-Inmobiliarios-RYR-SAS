307e79bef7fc8b428b82f74527974f32985a06c3
Alexander1230 <developergbp61@gmail.com>
2025-09-22 21:33:02 -0500

    feat: :bug: fix deploy

--- server/infraestructure/server/server.js
+++ server/infraestructure/server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const session = require('express-session');
@@ -6,22 +7,29 @@ const cookieParser = require('cookie-parser');
 const PropiedadRoutes = require('../../application/routes/PropiedadRouter');
 const InfoEspecifica = require('../../application/routes/InfoEspecificaRouter');
 const UsuarioRouter = require('../../application/routes/usuarioRouter');
+
 const createServer = () => {
     const app = express();
 
     const server = http.createServer(app);
 
+    const allowedOrigins = [
+        "http://localhost:5173",
+        process.env.FRONTEND_URL  // esta la defines en tu .env para producción
+    ];
+
     app.use(cors({
-        origin: ["http://localhost:5173"],
+        origin: allowedOrigins,
         credentials: true
     }));
 
     app.use(express.json());
     app.use(cookieParser());
 
+    // Rutas API
     app.use('/propiedades', PropiedadRoutes);
-    app.use('/InfoEspecifica', InfoEspecifica)
-    app.use('/usuario', UsuarioRouter)
+    app.use('/InfoEspecifica', InfoEspecifica);
+    app.use('/usuario', UsuarioRouter);
 
     app.use(session({
         secret: process.env.KEY_SECRET,
@@ -32,6 +40,14 @@ const createServer = () => {
             httpOnly: true,
         }
     }));
+
+    // Servir archivos estáticos del frontend React build
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+    // Para cualquier ruta no manejada por la API, enviar index.html para que React maneje el routing
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
 
     return server;
 };
